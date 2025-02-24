5d403df4c8dbfe280da5dceca4b8da8ff6dbfdd6
Abdel Elabd <abdelabd777@gmail.com>
2025-02-24 13:48:10 -0800

    cleanup

--- 04_ref_design/sequentializer.sv
+++ 04_ref_design/sequentializer.sv
@@ -27,11 +27,6 @@ module sequentializer #(
     output logic [PIXEL_BIT_WIDTH-1:0] m_axis_tdata
 );
 
-    //TODO: These two hard-coded for now 
-    // int STRIPE_ORDER [25:0] = '{12, 13, 11, 14, 10, 15, 9, 16, 8, 17, 7, 18, 6, 19, 5, 20, 4, 21, 3, 22, 2, 23, 1, 24, 0, 25}; 
-    // int LINE_ORDER [103:0] = '{48, 49, 50, 51, 52, 53, 54, 55, 44, 45, 46, 47, 56, 57, 58, 59, 40, 41, 42, 43, 60, 61, 62, 63, 36, 37, 38, 39, 64, 65, 66, 67, 32, 33, 34, 35, 68, 69, 70, 71, 28, 29, 30, 31, 72, 73, 74, 75, 24, 25, 26, 27, 76, 77, 78, 79, 20, 21, 22, 23, 80, 81, 82, 83, 16, 17, 18, 19, 84, 85, 86, 87, 12, 13, 14, 15, 88, 89, 90, 91, 8, 9, 10, 11, 92, 93, 94, 95, 4, 5, 6, 7, 96, 97, 98, 99, 0, 1, 2, 3, 100, 101, 102, 103};
-    int LINE_ORDER [103:0] = '{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103};
-    
     //////////////////////// Store data-burst ////////////////////////
     logic [PIXEL_BIT_WIDTH-1:0] pixel_buffer [0:PIXELS_PER_BURST-1];
     generate
@@ -49,7 +44,7 @@ module sequentializer #(
 
 
     //////////////////////// Finite State machine ////////////////////////
-    // assign s_axis_tready = 1'b1; //TODO: Comment this out
+    assign s_axis_tready = 1'b1; //TODO: Comment this out
 
     enum logic {LOAD_IN, STREAM_OUT} ps, ns;
     always_ff @(posedge clk) begin
@@ -62,7 +57,7 @@ module sequentializer #(
         case (ps)
 
             LOAD_IN: begin
-                s_axis_tready = 1'b1;
+                // s_axis_tready = 1'b1;
                 m_axis_tvalid = 1'b0;
                 if (s_axis_tvalid && s_axis_tready) ns = STREAM_OUT;
                 else ns = LOAD_IN;
@@ -70,7 +65,7 @@ module sequentializer #(
 
             STREAM_OUT: begin
                 // s_axis_tready = 1'b0;
-                // m_axis_tvalid = 1'b1;
+                m_axis_tvalid = 1'b1;
                 if (m_axis_tready && m_axis_tvalid) begin
                     if (cnt_idx_in_burst == PIXELS_PER_BURST-1) ns = LOAD_IN;
                     else ns = STREAM_OUT;
@@ -85,11 +80,9 @@ module sequentializer #(
     //////////////////////// Track indeces ////////////////////////
     logic [$clog2(PIXELS_PER_BURST)-1:0] cnt_idx_in_burst;
     logic [$clog2(IN_COLS)-1:0] cnt_col;
-    // logic [$clog2(IN_ROWS)-1:0] cnt_row_idx;
     logic [$clog2(IN_ROWS)-1:0] cnt_row;
     logic [$clog2(IN_COLS*IN_ROWS)-1:0] cnt_idx_in_frame;
     assign cnt_idx_in_frame = cnt_row*IN_COLS + cnt_col; // for testbenching and debugging
-    // assign cnt_row = LINE_ORDER[cnt_row_idx];
 
     always_ff @(posedge clk) begin
         if ((!s_axis_resetn)||srst||s_axis_tuser[0]) begin 
@@ -109,88 +102,5 @@ module sequentializer #(
 
         end
     end
-    
-
-    // always_ff @(posedge clk) begin
-
-    //     if ((!s_axis_resetn)||srst) begin 
-    //         // ps       <= LOAD_IN;
-    //         s_axis_tready  <= 1'b0;
-    //         m_axis_tvalid  <= 1'b0;
-    //         m_axis_tdata   <= '0;
-    //         cnt_idx_in_burst   <= 0;
-    //         cnt_row        <= '0;
-    //         cnt_col        <= '0;
-    //     end
-
-    //     else begin
-    //         // ps <= ns;
-
-    //         case (ps)
-    //             //-------------------------------------
-    //             LOAD_IN: begin
-    //                 s_axis_tready <= 1'b1;
-    //                 m_axis_tvalid <= 1'b0; 
-
-    //                 if (s_axis_tvalid) begin
-    //                     s_axis_tready <= 1'b0; 
-    //                     cnt_idx_in_burst <= 0;
-    //                 end
-    //             end
-
-    //             STREAM_OUT: begin
-    //                 s_axis_tready <= 1'b0;  
-    //                 m_axis_tvalid <= 1'b1;
-                    
-    //                 if (m_axis_tready) begin
-
-    //                     m_axis_tdata <= pixel_buffer[cnt_idx_in_burst];
-
-    //                     // Move to next pixel
-    //                     cnt_idx_in_burst <= cnt_idx_in_burst + 1'b1;
-                        
-    //                     // Increment col/row counters
-    //                     if (cnt_col == IN_COLS-1) begin
-    //                         cnt_col <= 0;
-    //                         if (cnt_row == IN_ROWS-1) cnt_row <= 0;
-    //                         else cnt_row <= cnt_row + 1'b1;
-    //                     end
-    //                     else cnt_col <= cnt_col + 1'b1;
-
-                        
-    //                 end
-    //             end
-                
-    //             default: begin
-    //             end
-    //         endcase
-    //     end
-    // end
-
-    // //-------------------------------------------------------------------------
-    // // Next-state logic
-    // //-------------------------------------------------------------------------
-    // always_comb begin
-    //     ns = ps;
-    //     case (ps)
-    //         LOAD_IN: begin
-    //             if (s_axis_tvalid) begin
-    //                 // Once we have a burst, go to output mode
-    //                 ns = STREAM_OUT;
-    //             end
-    //         end
-
-    //         STREAM_OUT: begin
-    //             // After outputting all 16 pixels, return to LOAD_IN
-    //             if (cnt_idx_in_burst == (PIXELS_PER_BURST-1) && m_axis_tready) begin
-    //                 ns = LOAD_IN;
-    //             end
-    //         end
-
-    //         default: begin
-    //             ns = LOAD_IN;
-    //         end
-    //     endcase
-    // end
 
 endmodule
