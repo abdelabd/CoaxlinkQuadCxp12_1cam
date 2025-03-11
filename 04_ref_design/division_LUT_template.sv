99b9180c12d86bdb92cdece9c3c64e848682b366
Abdel Elabd <abdelabd777@gmail.com>
2025-03-11 14:05:40 -0700

    fix generation of reciprocal-LUT to work with int-->frac mapping

--- 04_ref_design/division_LUT_template.sv
+++ 04_ref_design/division_LUT_template.sv
@@ -1,14 +1,14 @@
-module division_LUT_ap_fixed_{FP_TOTAL}_{FP_INT}_s{LUT_BITS} #(
+module udivision_LUT_{N_BITS}bit_int_to_{N_BITS}bit_frac #(
 )(
-    input logic [{FP_TOTAL}-1:0] number_in,
-    output logic [{FP_TOTAL}-1:0] reciprocal
+    input logic [{N_BITS}-1:0] number_in,
+    output logic [{N_BITS}-1:0] reciprocal
 
 );
 
-    logic [{NUM_ROWS-1}:0] reciprocal_LUT [{FP_TOTAL}-1:0];
+    logic [2**{N_BITS}-1:0] reciprocal_LUT [{N_BITS}-1:0];
 
     always_comb begin
-        case (number_in[{FP_TOTAL}-1:{FP_TOTAL}-{LUT_BITS}])
+        case (number_in)
 // INSERT LUT HERE
 
         endcase
