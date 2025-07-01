ccf65701436ab9daca3cadd221eebc609b532ae4
Abdel Elabd <abdelabd777@gmail.com>
2025-06-30 18:14:25 -0700

    regenerate axis_fifo

--- 07_vivado_project/CustomLogic.ip_user_files/ip/axis_fifo/axis_fifo_stub.vhdl
+++ 07_vivado_project/CustomLogic.ip_user_files/ip/axis_fifo/axis_fifo_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2022.2 (lin64) Build 3671981 Fri Oct 14 04:59:54 MDT 2022
--- Date        : Tue Jun 17 19:03:03 2025
+-- Date        : Mon Jun 30 18:09:38 2025
 -- Host        : DESKTOP-Q0UCNGC running 64-bit Ubuntu 24.04 LTS
 -- Command     : write_vhdl -force -mode synth_stub
 --               /home/aelabd/RHEED/CoaxlinkQuadCxp12_1cam/07_vivado_project/CustomLogic.gen/sources_1/ip/axis_fifo/axis_fifo_stub.vhdl
--- 07_vivado_project/CustomLogic.ip_user_files/sim_scripts/axis_fifo/xsim/cmd.tcl
+++ 07_vivado_project/CustomLogic.ip_user_files/sim_scripts/axis_fifo/xsim/cmd.tcl
@@ -8,5 +8,5 @@ if { [string length $curr_wave] == 0 } {
   }
 }
 
-run 100us
+run 20000us
 quit
